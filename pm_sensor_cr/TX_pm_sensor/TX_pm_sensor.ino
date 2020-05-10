6c9e89dd8d5a062ad394bf2bd5f80e94ff2d79de
Chris Rodell <crodell@eoas.ubc.ca>
2020-05-09 19:43:47 -0700

    fixed checksum error

--- pm_sensor_cr/TX_pm_sensor/TX_pm_sensor.ino
+++ pm_sensor_cr/TX_pm_sensor/TX_pm_sensor.ino
@@ -97,10 +97,7 @@ struct pms5003data data;
 
 void loop()
 {
-  delay(1000); // Wait 1 second between transmits, could also 'sleep' here!
-  Serial.println("Transmitting..."); // Send a message to rf95_server
-
-if (readPMSdata(&pmsSerial)) {
+  if (readPMSdata(&pmsSerial)) {
     // reading data was successful!
     Serial.println();
     Serial.println("---------------------------------------");
@@ -120,50 +117,53 @@ if (readPMSdata(&pmsSerial)) {
     Serial.print("Particles > 2.5um / 0.1L air:"); Serial.println(data.particles_25um);
     Serial.print("Particles > 5.0um / 0.1L air:"); Serial.println(data.particles_50um);
     Serial.print("Particles > 10.0 um / 0.1L air:"); Serial.println(data.particles_100um);
- 
-    String pm25 = String(data.pm25_env);
-    uint8_t pmlen = sizeof(pm25);
-    char radiopacket[20] = "Hello World #      ";
-    itoa(packetnum++, radiopacket+13, 10);
-    Serial.print("Sending "); Serial.println(radiopacket);
-    radiopacket[19] = 0;
+    Serial.println("---------------------------------------");
+  }
+
+ /*
+  delay(1000); // Wait 1 second between transmits, could also 'sleep' here!
+  */
+
+  Serial.println("Transmitting..."); // Send a message to rf95_server
   
-    Serial.println("Sending...");
-    delay(10);
-    rf95.send((uint8_t *)radiopacket, 20);
-
-    Serial.println("Waiting for packet to complete..."); 
-    delay(10);
-    rf95.waitPacketSent();
-    // Now wait for a reply
-    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-    uint8_t len = sizeof(buf);
-
-    Serial.println("Waiting for reply...");
-    if (rf95.waitAvailableTimeout(1000))
-    { 
-      // Should be a reply message for us now   
-      if (rf95.recv(buf, &len))
-      {
-        Serial.print("Got reply: ");
-        Serial.println((char*)buf);
-        Serial.print("RSSI: ");
-        Serial.println(rf95.lastRssi(), DEC);    
-     }
-     else
-     {
-        Serial.println("Receive failed");
-      }
+  char radiopacket[20] = "Hello World #      ";
+  itoa(packetnum++, radiopacket+13, 10);
+  Serial.print("Sending "); Serial.println(radiopacket);
+  radiopacket[19] = 0;
+  
+  Serial.println("Sending...");
+  delay(10);
+  rf95.send((uint8_t *)radiopacket, 20);
+ 
+  Serial.println("Waiting for packet to complete..."); 
+  delay(10);
+  rf95.waitPacketSent();
+  // Now wait for a reply
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+ 
+  Serial.println("Waiting for reply...");
+  if (rf95.waitAvailableTimeout(1000))
+  { 
+    // Should be a reply message for us now   
+    if (rf95.recv(buf, &len))
+   {
+      Serial.print("Got reply: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);    
     }
-  else
+    else
     {
-      Serial.println("No reply, is there a listener around?");
-     }
-
- }
+      Serial.println("Receive failed");
+    }
+  }
+  else
+  {
+    Serial.println("No reply, is there a listener around?");
+  }
+ 
 }
-
-
 
 
 /* PM */
