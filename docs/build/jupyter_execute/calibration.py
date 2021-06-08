2879647d4b90f563b84e6730b254fd6d1a61f270
Chris Rodell <crodell@eoas.ubc.ca>
2021-06-08 13:50:45 -0700

    rework to the sensor discription

--- docs/build/jupyter_execute/calibration.py
+++ docs/build/jupyter_execute/calibration.py
@@ -2,8 +2,9 @@
 # coding: utf-8
 
 # 
-# # Calibration
-# The calibration was done by using salt solutions with varied molar concentrations. The salt solution was fed into an atomizer providing air-born salt particles. The particles were forced through an adjustable filter enabling varied concentrations and into a 22L glass chamber where the UBC OPC sensors were located. The Grimm OPC sensor pulled air from the chamber to sample the salt concentrations as well. There was an exhaust port to maintain persistent salt concentration levels and a small fan within the chamber to mix the air. 
+# # Performance
+# We compared the low-cost UBC AQ sensors to a [GRIMM](https://www.grimm-aerosol.com/products-en/dust-monitors/the-dust-decoder/11-d/) OPC sensor (Model: 1.109). In a controlled laboratory setting, we produced air-borne salt particles by feeding salt solutions with known molar concentrations into an atomizer. From the atomizer, the air-borne salt particles went through an adjustable filter (enabling concentrations control) and a 22L glass chamber containing the UBC AQ sensors (Fig 1). The Grimm OPC sensor pulled air from the glass chamber to sample the salt concentrations. There was an exhaust port to maintain persistent salt concentration levels and a small fan within the glass chamber to mix the air. 
+# ![Figure 1](_static/img/chamber.jpeg)
 # 
 # The two OPC sensors were sampling at varied rates.
 # <br>
@@ -15,7 +16,6 @@
 # <br>
 # 
 # <br>
-# TODO add images of set up
 
 # In[1]:
 
@@ -34,7 +34,8 @@ from context import data_dir, save_dir
 
 
 # 
-# ## Lets call in the OPC data!
+# ## Call in the OPC data!
+# UBC AQ data was one minute averaged to match the GRIMM sample rate.
 
 # In[2]:
 
@@ -46,8 +47,8 @@ date_of_int = '20210430' # options 20210430 or 20210502
 
 def prepare_df(df):
   """
-  Function cleans UBC OPCs data by removeing duplicate headers and droping error values. 
-  Once cleaned takes 1 min avg of data to match GRIM sample rate
+  Function cleans UBC OPCs data by removing duplicate headers and dropping error values. 
+  Once cleaned takes 1 min avg of data to match GRIM sample rate.
   """
   df = df.drop(df[df.rtctime == 'rtctime'].index)
   df = df[~df['pm10_env'].str.contains('Rec')]
@@ -110,6 +111,7 @@ pylab.rcParams.update(params)
 
 
 # ### Plot PM 1.0 
+# Figure shows time series comparison of the measured PM 1.0 contractions of four UBC AQ Sensors [UBC-PM-01 (orange), UBC-PM-03 (green), UBC-PM-04 (red), UBC-PM-05 (blue)] and the GRIM sensor [GRIMM (blue)]. The time series shows one minute averaged PM 1.0 contractions incremented from 2021-04-30 09:27:00 unitl 2021-04-30 21:36:00. 
 
 # In[6]:
 
@@ -140,6 +142,7 @@ ax.legend(
 
 
 # ### Plot PM 2.5 
+# Figure shows time series comparison of the measured PM 2.5 contractions of four UBC AQ Sensors [UBC-PM-01 (orange), UBC-PM-03 (green), UBC-PM-04 (red), UBC-PM-05 (blue)] and the GRIM sensor [GRIMM (blue)]. The time series shows one minute averaged PM 1.0 contractions incremented from 2021-04-30 09:27:00 unitl 2021-04-30 21:36:00. 
 
 # In[7]:
 
@@ -168,6 +171,7 @@ ax.legend(
 
 
 # ### Plot PM 10 
+# Figure shows time series comparison of the measured PM 10 contractions of four UBC AQ Sensors [UBC-PM-01 (orange), UBC-PM-03 (green), UBC-PM-04 (red), UBC-PM-05 (blue)] and the GRIM sensor [GRIMM (blue)]. The time series shows one minute averaged PM 1.0 contractions incremented from 2021-04-30 09:27:00 unitl 2021-04-30 21:36:00. 
 
 # In[8]:
 
@@ -192,6 +196,51 @@ ax.legend(
     ncol=6,
     fancybox=True,
     shadow=True,
+)
+
+
+# ## Preliminary Results
+# We see the UBC-AQ sensors are performing marginally well at PM 1.0 concentrations. However,  the UBC-AQ sensors are grossly over-predicting the concentration levels of PM 2.5 and 10. Let's figure out why this is happening. 
+
+# ### Plot counts of particle sizes through time.
+# The figure shows the counts/liter of particles of size XX um and lower through time. 
+
+# In[9]:
+
+
+fig = plt.figure(figsize=(14, 6))
+fig.suptitle("Counts/liter of particles of size XX um and lower through time", fontsize=16)
+ax = fig.add_subplot(1, 1, 1)
+var_list = list(df_grim)[7:38]
+# var_list = list(df_grim)[7:16]
+for var in var_list:
+  ax.plot(df_grim['date & time'],df_grim[var], lw = 2.0, label = var[:-2])
+ax.set_ylabel('Counts/liter')
+ax.set_xlabel('Time (MM-DD HH)')
+ax.legend(
+  bbox_to_anchor=(1.2, 1.2),
+)
+
+
+# ### Plot counts of particle sizes averaged over time.
+# The figure shows time averaged counts/liter of particles of size XX um and lower. 
+
+# In[10]:
+
+
+fig = plt.figure(figsize=(14, 6))
+fig.suptitle("Time averaged counts/liter of particles of size XX um and lower", fontsize=16)
+ax = fig.add_subplot(1, 1, 1)
+var_list = list(df_grim)[7:38]
+var_labels = [var[:-2] for var in var_list]
+df_grim_mean = df_grim[var_list].mean()
+
+ax.bar(var_labels,df_grim_mean)
+ax.set_ylabel('Counts/liter')
+ax.set_xlabel('Particles of size XX um and lower')
+ax.set_xticklabels(var_labels, rotation = 45)
+ax.legend(
+  bbox_to_anchor=(1.2, 1.2),
 )
 
 
